e2d3b96777b6d2c1e7a7eec29205b44ac0210304
kmilom <90485091+kmilom@users.noreply.github.com>
2024-05-14 15:54:40 -0500

    axios and react-router-dom were added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,12 +8,12 @@ export default function Home(){
     return(
         <div className="home">
             <Row>
-                <Col xs = { 12 }>
+                <Col xs = { 12 } >
                     <HeaderHome />
                 </Col>
             </Row>
             <Row>
-                <Col xs = { 12 }>
+                <Col xs = { 12 } className="py-1" id = "box">
                     <LandingPage/>
                 </Col>
             </Row>
